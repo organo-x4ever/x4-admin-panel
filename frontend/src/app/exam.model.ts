e050ea2edc0073187a0bb9c1454e7d80c8f2a1b4
Gurpreet Deol <gurpreet.deol@oghq.ca>
2019-09-20 20:51:48 +0100

    integrating Flask and Angular

---
+++ frontend/src/app/exam.model.ts
@@ -0,0 +1,9 @@
+export class Exam {
+  title: string;
+  description: string;
+  // tslint:disable-next-line:variable-name
+  id: number;
+  updatedAt?: Date;
+  createdAt?: Date;
+  lastUpdatedBy?: string;
+}
